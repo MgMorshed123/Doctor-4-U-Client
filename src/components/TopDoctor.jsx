3c0e1c8e875a9f32f07c849bd5cea0cce42692a6
mdarf <mdarfinji45@gmail.com>
2024-12-03 01:46:34 +0800

    card HoverEffect added

--- src/components/TopDoctor.jsx
+++ src/components/TopDoctor.jsx
@@ -8,10 +8,13 @@ const TopDoctor = () => {
       <p className="sm:w-1/3 text-center text-sm">
         Simply browse through our extensive list of trusted doctors.
       </p>
-      <div>
+      <div className="w-full grid grid-cols-auto gap-4 gap-y-6 px-3 sm:px-3">
         {doctors.slice(0, 10).map((item, index) => {
           return (
-            <div key={index} className="flex ">
+            <div
+              className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] translate-all duration-500"
+              key={index}
+            >
               <img src={item.image} alt="" srcset="" />
               <div>
                 <div>
