77d70a6bf4924c36b80be3eaa7bee59c743c0b16
mdarf <mdarfinji45@gmail.com>
2024-12-03 02:40:50 +0800

    creating banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { assets } from "../assets/assets_frontend/assets";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div>
+      {/* left */}
+      <div>
+        <div>
+          <p>Book Appointment</p>
+          <p>With 100+ Trusted Doctors</p>
+        </div>
+        <button>Create Appointment</button>
+      </div>
+
+      {/* right */}
+
+      <div>
+        <img src={assets.appointment_img} alt="" srcset="" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
