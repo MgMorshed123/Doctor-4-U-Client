bfc8e032eff5e77cb47a4c09c4d296e87207e467
mdarf <mdarfinji45@gmail.com>
2024-12-02 21:17:22 +0800

    some changes in route

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,13 @@ const App = () => {
     <div className="mx-4 sm:mx-[10%]">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/myprofile" element={<MyProfile />} />
-        <Route path="/myappointments" element={<MyAppointments />} />
         <Route path="/doctors" element={<Doctors />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
       </Routes>
     </div>
   );
