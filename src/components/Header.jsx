88c1306e91525c1595c79cda24300816e7b57407
mdarf <mdarfinji45@gmail.com>
2024-12-03 00:52:09 +0800

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,23 +3,27 @@ import { assets } from "../assets/assets_frontend/assets";
 
 const Header = () => {
   return (
-    <div className=" flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10  lg:px-20">
+    <div className=" flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10  lg:px-20 mt-5">
       {/* left side  */}
 
-      <div>
-        <p>
+      <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 md:py-[10vw] md:mb-[-30px]">
+        <p className="text-3xl md:text-4xl  lg:text-4xl text-white font-semibold md:leading-tight leading-tight lg:leading-tight">
           {" "}
           Book Appointment <br />
           With Trusted Doctors
         </p>
-        <div>
+        <div className="flex flex-col md:flex-row items-center gap-3 text-white font-light ">
           <img src={assets.group_profiles} alt="" srcset="" />
           <p>
-            Simply browse through our extensive list of trusted doctors, <br />
+            Simply browse through our extensive list of trusted doctors,{" "}
+            <br className="hidden md:block" />
             schedule your appointment hassle-free.
           </p>
         </div>
-        <a href="">
+        <a
+          className="flex items-center gap-2 bg-white px-8 py-3 rounded-full"
+          href="#speciality"
+        >
           Book Appointment
           <img src={assets.arrow_icon} className="w-4" alt="" srcset="" />
         </a>
@@ -27,8 +31,13 @@ const Header = () => {
 
       {/*  right side  */}
 
-      <div>
-        <img src={assets.header_img} alt="" srcset="" />
+      <div className="md:w-1/2 relative">
+        <img
+          className="w-full md:absolute bottom-0 h-auto rounded-lg"
+          src={assets.header_img}
+          alt=""
+          srcset=""
+        />
       </div>
     </div>
   );
