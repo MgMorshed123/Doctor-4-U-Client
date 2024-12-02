61bef032928906c22ab03e90890d739982ae67ee
mdarf <mdarfinji45@gmail.com>
2024-12-03 01:05:17 +0800

    speciality name image added

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -1,7 +1,31 @@
 import React from "react";
-
+import { specialityData } from "../assets/assets_frontend/assets";
+import { Link } from "react-router-dom";
 const SpecialityMenu = () => {
-  return <div>SpecialityMenu</div>;
+  return (
+    <div
+      id="speciality"
+      className="flex flex-col items-center gap-4 py-16 text-gray-800"
+    >
+      <h1 className="text-3xl font-medium">Find By Speciality</h1>
+      <p className="sm:w-1/3 text-center text-sm">
+        Simply browse through our extensive list of trusted doctors, schedule
+        your appointment hassle-free.
+      </p>
+      <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll">
+        {specialityData.map((item, index) => {
+          return (
+            <Link to={`/doctors/${item.speciality}`} key={index}>
+              <img src={item.image} alt="" srcset="" />
+              <p>{item.speciality}</p>
+            </Link>
+          );
+        })}
+      </div>
+
+      <div></div>
+    </div>
+  );
 };
 
 export default SpecialityMenu;
