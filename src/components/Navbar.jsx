57a67380f22df921dfcce8e48118465955928f42
mdarf <mdarfinji45@gmail.com>
2024-12-02 21:35:42 +0800

    active hr added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,23 +5,23 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="flex justify-between  items-center text-sm py-4 border-b  border-gray-400">
-      <img src={assets.logo} alt="" srcset="" />
-      <ul>
-        <NavLink>
-          <li>Home</li>
-          <hr />
+      <img className="w-44 cursor-pointer" src={assets.logo} alt="" srcset="" />
+      <ul className="hidded  md:flex items-start gap-5 font-medium ">
+        <NavLink to="/">
+          <li className="py-2">Home</li>
+          <hr className="border-none h-0.5 bg-primary w-3/5  m-auto hidden" />
         </NavLink>
-        <NavLink>
-          <li>All Doctors </li>
-          <hr />
+        <NavLink to="/doctors">
+          <li className="py-2">All Doctors </li>
+          <hr className="border-none h-0.5 bg-primary w-3/5  m-auto hidden" />
         </NavLink>
-        <NavLink>
-          <li>About</li>
-          <hr />
+        <NavLink to="/about">
+          <li className="py-2">About</li>
+          <hr className="border-none h-0.5 bg-primary w-3/5  m-auto hidden" />
         </NavLink>
-        <NavLink>
-          <li>Contact</li>
-          <hr />
+        <NavLink to="/contact">
+          <li className="py-2">Contact</li>
+          <hr className="border-none h-0.5 bg-primary w-3/5  m-auto hidden" />
         </NavLink>
       </ul>
       <button>Create Account</button>
