09d82b80934b36e49445564aded1336875a69d1f
mdarf <mdarfinji45@gmail.com>
2024-12-02 21:13:24 +0800

    added basic route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import MyProfile from "./pages/MyProfile";
+import MyAppointments from "./pages/MyAppointments";
+import Doctors from "./pages/Doctors";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/myprofile" element={<MyProfile />} />
+        <Route path="/myappointments" element={<MyAppointments />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
