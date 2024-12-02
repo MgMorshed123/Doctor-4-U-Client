bf51a328c7947cf74e441af46600a13fa5faf1f4
mdarf <mdarfinji45@gmail.com>
2024-12-03 01:35:17 +0800

    TopDoctor.jsx adding css

--- src/components/TopDoctor.jsx
+++ src/components/TopDoctor.jsx
@@ -3,9 +3,11 @@ import { doctors } from "../assets/assets_frontend/assets";
 
 const TopDoctor = () => {
   return (
-    <div>
-      <h1>Top Doctors to Book</h1>
-      <p>Simply browse through our extensive list of trusted doctors.</p>
+    <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
+      <h1 className="text-3xl font-medium ">Top Doctors to Book</h1>
+      <p className="sm:w-1/3 text-center text-sm">
+        Simply browse through our extensive list of trusted doctors.
+      </p>
       <div>
         {doctors.slice(0, 10).map((item, index) => {
           return (
