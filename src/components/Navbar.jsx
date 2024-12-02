6cf96a23517b08cdf037953b53d41908f5e3d9cb
mdarf <mdarfinji45@gmail.com>
2024-12-02 22:36:02 +0800

    profile icon added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-
+import { assets } from "../assets/assets_frontend/assets";
 const Navbar = () => {
   const navigate = useNavigate();
 
+  const [showMenu, setShowMenu] = useState(false);
+  const [token, setToken] = useState(true);
+
   return (
     <div className="flex justify-between  items-center text-sm py-4 border-b  border-gray-400">
-      <img to="/" className="w-44 cursor-pointer" src={""} alt="" srcset="" />
+      <NavLink to="/">
+        <img
+          to="/"
+          className="w-40 h-20 cursor-pointer "
+          src={assets.logo2}
+          alt=""
+          srcset=""
+        />
+      </NavLink>
       <ul className="hidded  md:flex items-start gap-5 font-medium ">
         <NavLink to="/">
           <li className="py-2">Home</li>
@@ -26,12 +37,31 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className="flex items-center gap-4">
-        <button
-          onClick={() => navigate("/login")}
-          className=" bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block"
-        >
-          Create Account
-        </button>
+        {token ? (
+          <div className="flex items-center gap-2 cursor-pointer group relative">
+            <img
+              className="w-8 rounded-full"
+              src={assets.profile_pic}
+              alt=""
+              srcset=""
+            />
+            <img
+              className="w-2.5"
+              src={assets.dropdown_icon}
+              alt=""
+              srcset=""
+            />
+          </div>
+        ) : (
+          <div>
+            <button
+              onClick={() => navigate("/login")}
+              className=" bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block"
+            >
+              Create Account
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
