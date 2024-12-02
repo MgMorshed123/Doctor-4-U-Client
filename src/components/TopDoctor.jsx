dd3d5d07024b9dda8447dab9171a1c64f21cd0cb
mdarf <mdarfinji45@gmail.com>
2024-12-03 01:24:34 +0800

    TopDoctor  list added

--- src/components/TopDoctor.jsx
+++ src/components/TopDoctor.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { doctors } from "../assets/assets_frontend/assets";
 
 const TopDoctor = () => {
   return (
     <div>
       <h1>Top Doctors to Book</h1>
       <p>Simply browse through our extensive list of trusted doctors.</p>
-      <div></div>
+      <div>
+        {doctors.slice(0, 3).map((item, index) => {
+          return (
+            <div key={index} className="flex gap-3">
+              <img src={item.image} alt="" srcset="" />
+              <div>
+                <p>{item.name}</p>
+                <p>{item.speciality}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
