99634c4fc278b78ad41c97322718761c21ebb026
mdarf <mdarfinji45@gmail.com>
2024-12-09 17:00:32 +0800

    getting doctere details

--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const Appointments = () => {
+  const { docId } = useParams();
+  const { doctors } = useContext(AppContext);
+
+  const [docInfo, setDocInfo] = useState(null);
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find((doc) => doc._id === docId);
+    setDocInfo(docInfo);
+  };
+
   return <div>Appointments</div>;
 };
 
