48b5cf6322fc720848decca548e34a4b293fb811
mdarf <mdarfinji45@gmail.com>
2024-12-09 13:51:35 +0800

    banner text css added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,11 +6,11 @@ const Banner = () => {
     <div className="flex bg-primary rounded-lg px-6 sm:px-10 lg:px-12 my-20 md:mx-20">
       {/* left */}
       <div className="flex-1 py-8  md:py-16 lg:py-24  lg:pl-5 ">
-        <div>
+        <div className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-semibold text-white ">
           <p>Book Appointment</p>
           <p>With 100+ Trusted Doctors</p>
         </div>
-        <button>Create Appointment</button>
+        <button className="">Create Appointment</button>
       </div>
 
       {/* right */}
