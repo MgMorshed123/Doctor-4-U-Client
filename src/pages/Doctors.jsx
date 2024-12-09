a7e1c59b0579809e77507eff1515b8f0fc7bf17e
mdarf <mdarfinji45@gmail.com>
2024-12-09 15:33:21 +0800

    speciality tab added

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -25,35 +25,66 @@ const Doctors = () => {
 
   return (
     <div>
-      <p>Browse through the doctors specialist.</p>
-      <div>
-        <div>General physician</div>
-        <div>Gynecologist</div>
-        <div>Dermatologist</div>
-        <div>Pediatricians</div>
-        <div>Neurologist</div>
-        <div>Gastroenterologist</div>
-      </div>
-      <div>
-        {filterDoc.map((item, index) => {
-          return (
-            <div
-              onClick={() => navigate(`/appointment/${item._id}`)}
-              className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] translate-all duration-500"
-              key={index}
-            >
-              <img className="bg-blue-50" src={item.image} alt="" srcset="" />
-              <div className="p-4">
-                <div className="flex items-center gap-2 text-sm text-green-700 ">
-                  <p className="w-2 h-2 rounded-full bg-green-700"></p>
-                  <p className="text-green-700">Available</p>
+      <p className="text-gray-600 flex flex-col sm:flex-row items-start gap-5 mt-5 ">
+        Browse through the doctors specialist.
+      </p>
+      <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
+        <div className=" flex flex-col gap-4 text-gray-600 ">
+          <p
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}
+          >
+            General physician
+          </p>
+          <p
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}
+          >
+            Gynecologist
+          </p>
+          <p
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}
+          >
+            Dermatologist
+          </p>
+          <p
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}
+          >
+            Pediatricians
+          </p>
+          <p
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}
+          >
+            Neurologist
+          </p>
+          <p
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}
+          >
+            Gastroenterologist
+          </p>
+        </div>
+
+        <div className="w-full grid grid-cols-auto gap-4 gap-y-6">
+          {filterDoc.map((item, index) => {
+            return (
+              <div
+                onClick={() => navigate(`/appointment/${item._id}`)}
+                className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] translate-all duration-500"
+                key={index}
+              >
+                <img className="bg-blue-50" src={item.image} alt="" srcset="" />
+                <div className="p-4">
+                  <div className="flex items-center gap-2 text-sm text-green-700 ">
+                    <p className="w-2 h-2 rounded-full bg-green-700"></p>
+                    <p className="text-green-700">Available</p>
+                  </div>
+                  <p className="text-gray-900 text-lg font-medium">
+                    {item.name}
+                  </p>
+                  <p className="text-gray-600 text-sm ">{item.speciality}</p>
                 </div>
-                <p className="text-gray-900 text-lg font-medium">{item.name}</p>
-                <p className="text-gray-600 text-sm ">{item.speciality}</p>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
