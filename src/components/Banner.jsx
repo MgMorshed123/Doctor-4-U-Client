0f3d0edc05445e11b476586c7674f9152ddbf52f
mdarf <mdarfinji45@gmail.com>
2024-12-09 13:47:10 +0800

    addning banner css

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,9 +3,9 @@ import { assets } from "../assets/assets_frontend/assets";
 
 const Banner = () => {
   return (
-    <div>
+    <div className="flex bg-primary rounded-lg px-6 sm:px-10 lg:px-12 my-20 md:mx-20">
       {/* left */}
-      <div>
+      <div className="flex-1 py-8  md:py-16 lg:py-24  lg:pl-5 ">
         <div>
           <p>Book Appointment</p>
           <p>With 100+ Trusted Doctors</p>
@@ -15,8 +15,13 @@ const Banner = () => {
 
       {/* right */}
 
-      <div>
-        <img src={assets.appointment_img} alt="" srcset="" />
+      <div className="hidden md:block md:w-1/2  lg:w-[370px] relative">
+        <img
+          className="w-full absolute bottom-0 right-0 max-w-md"
+          src={assets.appointment_img}
+          alt=""
+          srcset=""
+        />
       </div>
     </div>
   );
