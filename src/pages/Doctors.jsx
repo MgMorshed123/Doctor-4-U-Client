8b15488d21713ae4002fecd7d4cc3d3494c298ce
mdarf <mdarfinji45@gmail.com>
2024-12-09 14:52:52 +0800

    adding speciality

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Doctors = () => {
-  return <div>Doctors</div>;
+  const { speciality } = useParams;
+  console.log(speciality);
+
+  const { doctors } = useContext(AppContext);
+
+  return (
+    <div>
+      <p>Browse through the doctors specialist.</p>
+      <div>
+        <div>General physician</div>
+        <div>Gynecologist</div>
+        <div>Dermatologist</div>
+        <div>Pediatricians</div>
+        <div>Neurologist</div>
+        <div>Gastroenterologist</div>
+      </div>
+    </div>
+  );
 };
 
 export default Doctors;
