3bc4422727c2388951359b8242f832d6b30b1776
mdarf <mdarfinji45@gmail.com>
2024-12-14 15:34:40 +0800

    contact us done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,20 +10,29 @@ const Contact = () => {
         </p>
       </div>
 
-      <div>
-        <img src={assets.contact_image} alt="" srcset="" />
-        <div>
-          <p>OUR OFFICE</p>
-          <p>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10  mb-28 text-sm">
+        <img
+          className="w-full md:max-w-[360px]"
+          src={assets.contact_image}
+          alt=""
+          srcset=""
+        />
+        <div className="flex flex-col justify-center item-start gap-6">
+          <p className="font-semibold text-lg text-gray-600 ">OUR OFFICE</p>
+          <p className="text-gray-600">
             00000 Willms Station
             <br />
           </p>
 
-          <p>
+          <p className="text-gray-600">
             Email: greatstackdev@gmail.com <br /> USA Tel: (000) 000-0000
           </p>
-          <p>Learn more about our teams and job openings.</p>
-          <button>Explore jobs </button>
+          <p className="text-gray-600">
+            Learn more about our teams and job openings.
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explore jobs
+          </button>
         </div>
       </div>
     </div>
