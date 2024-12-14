9f7fd2559ce667292c994491ae247098f0a676a1
mdarf <mdarfinji45@gmail.com>
2024-12-14 18:02:49 +0800

    adding profile edit functionality

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { assets } from "../assets/assets_frontend/assets";
 
 const MyProfile = () => {
-  return <div>MyProfile</div>;
+  const [userData, setUserDate] = useState({
+    name: "Edward",
+    image: assets.profile_pic,
+    email: "mdarfinji45@gmail.com",
+    phone: "+601123989642",
+    address: {
+      line1: "7th street",
+      line2: "Circle ,London",
+    },
+    gender: "Male",
+    don: "2000-01-20",
+  });
+
+  const [isEdit, setEdit] = useState(false);
+
+  return (
+    <div>
+      <img src={userData.image} alt="" srcset="" />
+      {isEdit ? (
+        <input
+          type="text"
+          onChange={(e) =>
+            setUserDate((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+      ) : (
+        <p></p>
+      )}
+      <hr />
+      <div>
+        <p>Contact Information</p>
+        <div>
+          <p>Email id :</p>
+          <p>{userData.email}</p>
+          <p>Phone : </p>
+          {isEdit ? (
+            <input
+              type="text"
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, name: e.target.value }))
+              }
+            />
+          ) : (
+            <p>{userData.phone}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyProfile;
