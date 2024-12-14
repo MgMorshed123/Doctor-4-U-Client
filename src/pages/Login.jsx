695d28a3d7c7977ed0466eb5af21327010c23f63
mdarf <mdarfinji45@gmail.com>
2024-12-14 15:54:05 +0800

    seting up login and sign up page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [state, setState] = useState("Sign Up");
+
+  const [email, setEmail] = useState();
+  const [password, setpassword] = useState();
+  const [name, setName] = useState();
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form className="min-h-[80vh] flex-items-center">
+      <div>
+        <p>{state == "Sign Up" ? "Create Account " : "Login"}</p>
+        <p>
+          Please {state == "Sign Up" ? "Sign Up" : "Login"} to book Appointment{" "}
+        </p>
+        <div>
+          <p>Full Name </p>
+          <input
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </div>
+
+        <div>
+          <p>Email </p>
+          <input
+            type="email"
+            onChange={(e) => setName(e.target.value)}
+            value={email}
+          />
+        </div>
+
+        <div>
+          <p>Password </p>
+          <input
+            type="password"
+            onChange={(e) => setName(e.target.value)}
+            value={password}
+          />
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
