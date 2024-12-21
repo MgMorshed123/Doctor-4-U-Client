793575988b8df34b6170fdb10553637de0f06d0a
mdarf <mdarfinji45@gmail.com>
2024-12-21 14:44:51 +0800

    implementing login api

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,17 +1,50 @@
-import { createContext } from "react";
-import { doctors } from "../assets/assets_frontend/assets";
+import { createContext, useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import axios from "axios";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "$";
 
-  const values = {
-    doctors,
-    currencySymbol,
+  const [doctors, setDoctors] = useState([]);
+  const [token, setToken] = useState("");
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const getDoctorsData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/doctor/list");
+      console.log(data);
+      if (data.success) {
+        setDoctors(data.doctors);
+      } else {
+        Swal.fire({
+          title: data.message,
+          // text: "You clicked the button!",
+          icon: "success",
+        });
+      }
+    } catch (error) {
+      Swal.fire({
+        title: error.message,
+        // text: "You clicked the button!",
+        icon: "success",
+      });
+    }
   };
 
-  console.log(values);
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
+
+  const values = {
+    doctors,
+    token,
+    setToken,
+    currencySymbol,
+    backendUrl,
+  };
 
   return (
     <AppContext.Provider value={values}>{props.children}</AppContext.Provider>
